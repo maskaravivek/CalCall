6a8b0364c251adadac698085af4352079225efdf
utkarsha bakshi <utkarsha.bakshi@gmail.com>
2021-08-08 21:16:08 -0700

    Sync calendars

--- pages/ContactListPage/index.js
+++ pages/ContactListPage/index.js
@@ -150,16 +150,11 @@ const styles = StyleSheet.create({
 
 const getAvatarInitials = textString => {
     if (!textString) return "";
-
     const text = textString.trim();
-
     const textSplit = text.split(" ");
-
     if (textSplit.length <= 1) return text.charAt(0);
-
     const initials =
         textSplit[0].charAt(0) + textSplit[textSplit.length - 1].charAt(0);
-
     return initials;
 };
 
